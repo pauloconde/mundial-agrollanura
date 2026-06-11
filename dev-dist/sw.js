bb52aa5afa36229510b69a53b34e2eb57951dead
Paulo Conde <pauloconde@gmail.com>
2026-06-11 02:43:14 -0400

    qqq

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.b7chr8qiq3o"
+    "revision": "0.8cnrhv3rg8"
   }], {
     "directoryIndex": "index.html"
   });
